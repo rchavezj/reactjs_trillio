2505bb86b2375165d078301f9706da5a2c1e5a35
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-06-11 22:34:10 -0700

    Deleted old header file

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -9,9 +9,7 @@ const Header = () => {
     return (
         <header className="header">
             <img src={logo} alt="trillo logo" className="logo" />
-
             <Search />
-
             <UserNav />
         </header >
     )
